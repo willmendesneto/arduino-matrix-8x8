7d6c8caae1100465068e76abfc90041dd4da3a56
willmendesneto <willmendesneto@gmail.com>
2013-06-28 21:28:19 -0700

    Example ok with sensors:
    - luminosity;
    - temperature;
    - ultrasonic;
    
    Signed-off-by: willmendesneto <willmendesneto@gmail.com>

--- apresentacao/apresentacao_unificada/apresentacao_unificada.ino
+++ apresentacao/apresentacao_unificada/apresentacao_unificada.ino
@@ -2,21 +2,49 @@
 #include <LEDMatrix.h>
 #include <Thermistor.h>
 
+
+//  usado para definir os pinos usados no sensor ultrasonico
+// # Connection:
+// #       SENSOR ULTRA-SONICO  -> ARDUINO
+// #       Pin 1 VCC (URM V3.2) -> VCC (Arduino)
+// #       Pin 2 GND (URM V3.2) -> GND (Arduino)
+// #       Pin 4 PWM (URM V3.2) -> Pin 3 (Arduino)
+// #       Pin 6 COMP/TRIG (URM V3.2) -> Pin 5 (Arduino)
+// #
+int URPWM = 3; // PWM Output 0－25000US，Every 50US represent 1cm
+int URTRIG = 5; // PWM trigger pin
+
+// usado para definir pino para o sensor de temperatura
 Thermistor temp(17);
 
-LEDMatrix mymatriz('C');
+uint8_t EnPwmCmd[4] = {0x44, 0x02, 0xbb, 0x01};    // distance measure command
+//  funcao para passar valor inicial para o sensor ultrasonico
+unsigned int distance = 0;
 
-char numbers[10] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+// usado para imprimir na matriz
+LEDMatrix mymatriz('C');
 
 void setup(){
     Serial.begin(9600);
+    
+    //  Chamando funcao usada para o sensor ultrasonico
+    PWM_Mode_Setup();
+    
+    //  Inicializa a matriz de led
     mymatriz.begin(10, 11, 12, 13, 14, 15, 4, 6, 7, 8, 9);
 }
 
 void loop(){    
   
-        String temperatura = Termistor();
-        printValue(temperatura);
+        //String distance = PWM_Mode();
+        //printValue(distance);    
+        
+        //String temperatura = Termistor();
+        //printValue(temperatura);
+        
+        
+        //String luminosidade = getValueSensorLDR(16);
+        //printValue(luminosidade);
 }
 
 //Printing the values in matrix
@@ -50,4 +78,53 @@ String Termistor ()
   Serial.println("*C");
   
   return temperature;
+}
+
+//  funcao usada para o sensor de luminosidade
+String getValueSensorLDR(int numeroPino)
+{
+  String value = String(analogRead(numeroPino));
+  Serial.print(value); 
+  Serial.println (" luminosidade");
+  return value;
+}
+
+//  funcao usada para o sensor ultrasonico
+void PWM_Mode_Setup(){
+  pinMode(URTRIG, OUTPUT);                     // A low pull on pin COMP/TRIG
+  digitalWrite(URTRIG, HIGH);                  // Set to HIGH
+
+  pinMode(URPWM, INPUT);                      // Sending Enable PWM mode command
+
+  for(int i=0; i<4; i++){
+      Serial.write(EnPwmCmd[i]);
+  }
+  
+  //  Initial message in serial
+  Serial.println("\n\nThen we go!!\n");
+}
+
+//  funcao usada para o sensor ultrasonico
+String PWM_Mode(){              
+
+  // a low pull on pin COMP/TRIG  triggering a sensor reading
+  digitalWrite(URTRIG, LOW);
+  digitalWrite(URTRIG, HIGH);               // reading Pin PWM will output pulses
+
+  unsigned long DistanceMeasured = pulseIn(URPWM, LOW);
+
+  if(DistanceMeasured == 50000){              // the reading is invalid.
+    Serial.print("Distancia invalida!");
+    distance = 0;
+  } else {
+    distance = DistanceMeasured / 50;           // every 50us low level stands for 1cm
+  }
+    
+  String numberPrintable = String(distance);
+    
+  //  Printing values in serial
+  Serial.print(numberPrintable);
+  Serial.println("(cm)");
+    
+  return numberPrintable;
 }
